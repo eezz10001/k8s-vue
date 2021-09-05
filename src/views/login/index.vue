be04f90054e950004ede5ecb68f4fce1a348c7c2
GrootYuan <8612707+grootyuan@user.noreply.gitee.com>
2021-09-05 23:09:53 +0800

    first commit

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="login-container">
+
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">Cloud Platform</h3>
       </div>
 
       <el-form-item prop="username">
@@ -42,11 +43,6 @@
       </el-form-item>
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
 
     </el-form>
   </div>
---
+++ src/views/workloads/deplist.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="app-container">
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column align="center" label="ID" width="95">
+        <template slot-scope="scope">
+          {{ scope.$index+1 }}
+        </template>
+      </el-table-column>
+      <el-table-column label="名称" width="300">
+        <template slot-scope="scope">
+          {{ scope.row.Name }}
+        </template>
+      </el-table-column>
+      <el-table-column label="命名空间" width="110" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.NameSpace }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="镜像" width="150" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.Images }}
+        </template>
+      </el-table-column>
+
+    </el-table>
+  </div>
+</template>
+
+<script>
+import { getList } from '@/api/deployments'
+import { NewClient } from '@/utils/ws'
+
+export default {
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      wsClient: null
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      // 通过rest api 获取
+      getList('myweb').then(response => {
+        this.list = response.data
+        this.listLoading = false
+      })
+      this.wsClient = NewClient()
+      this.wsClient.onmessage = (e) => {
+        if (e.data !== 'ping') {
+          this.list = JSON.parse(e.data)
+          this.$forceUpdate()
+        }
+      }
+    }
+  }
+}
+</script>
