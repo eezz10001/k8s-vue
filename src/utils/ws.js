cc6c05ffb1d80d8a97871071dd7468e4d8e22121
GrootYuan <8612707+grootyuan@user.noreply.gitee.com>
2021-09-06 09:25:13 +0800

    x

--- src/utils/ws.js
+++ src/utils/ws.js
@@ -1,16 +1,15 @@
-const NewClient=function () {
-    var ws = new WebSocket("ws://localhost:8080/ws");
-    ws.onopen = function(){
-        console.log("open");
-    }
-    ws.onclose = function(e){
-        console.log("close");
-    }
-    ws.onerror = function(e){
-
-        console.log(e);
-    }
-    return ws
+const NewClient = function() {
+  var ws = new WebSocket('ws://localhost:8080/ws')
+  ws.onopen = function() {
+    console.log('open')
+  }
+  ws.onclose = function(e) {
+    console.log('close')
+  }
+  ws.onerror = function(e) {
+    console.log(e)
+  }
+  return ws
 }
-export {NewClient}
+export { NewClient }
 
